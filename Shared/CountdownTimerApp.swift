78f9b969657068a537479bf78a2e22d53c528bad
Theo <thodorispalios1@hotmail.com>
2021-03-28 18:49:50 +0300

    Initial commit

---
+++ Shared/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  Shared
+//
+//  Created by Theo on 24/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
